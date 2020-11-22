6f86490272f9f3cc65baa4ef94b819dd6338f895
Mohit Patil <mohitpatil546@gmail.com>
2020-11-23 03:38:01 +0530

    - environment apiUrl updated to backend service running on skuad

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,6 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+  // apiUrl: 'http://localhost:8080'
+  apiUrl: 'http://54.82.30.170:4000' // commiting this change for testing
 };
