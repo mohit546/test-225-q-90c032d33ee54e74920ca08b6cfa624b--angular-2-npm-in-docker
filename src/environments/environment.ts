5692c8709fb3c008f90dba9009e8737d00558e8a
Mohit Patil <mohitpatil546@gmail.com>
2020-11-23 04:36:43 +0530

    - restored environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,5 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:8080'
-  apiUrl: 'http://54.82.30.170:4000' // commiting this change for testing
+  apiUrl: 'http://localhost:8080'
 };
